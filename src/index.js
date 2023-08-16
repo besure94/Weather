d1363422f533d35a0a34857b95a3c2013aaa49b5
Brian Scherner <brianscherner@gmail.com>
2023-08-16 10:19:46 -0700

    Add promise and update printElements and printError functionality.

--- src/index.js
+++ src/index.js
@@ -4,28 +4,34 @@ import './css/styles.css';
 
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    console.log(response);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+    request.addEventListener("loadend", function() {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, city]);
+      } else {
+        reject([this, response, city]);
+      }
+    });
+    request.open("GET", url, true);
+    request.send();
   });
 
-  request.open("GET", url, true);
-  request.send();
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
 }
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printElements(results) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${results[1]} is ${results[0].main.humidity}%. The temperature is ${results[0].main.temp} degrees Fahrenheit. The temperature feels like ${results[0].main.feels_like} degrees. The low for today is ${results[0].main.temp_min} degrees, and the high for today is ${results[0].main.temp_max} degrees. The wind speed is ${results[0].wind.speed} miles per hour.`;
 }
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. The temperature is ${apiResponse.main.temp} degrees Fahrenheit. The temperature feels like ${apiResponse.main.feels_like} degrees. The low for today is ${apiResponse.main.temp_min} degrees, and the high for today is ${apiResponse.main.temp_max} degrees. The wind speed is ${apiResponse.wind.speed} miles per hour.`;
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}`;
 }
 
 function handleFormSubmission(event) {
